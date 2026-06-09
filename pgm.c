907ce8fa43dcce5ddfc2ae46eec59c120098da12
vitorhenriquec0 <vitorhenriquecardoso59@gmail.com>
2026-06-09 18:38:49 -0300

    project completed

--- pgm.c
+++ pgm.c
@@ -9,7 +9,7 @@ Imagem *ler_pgm(const char *arquivo)
 	FILE *f = fopen(arquivo, "r");
 	if (!f) 
 	{
-		fprintf(stderr, "[ERRO] ler_pgm: não abriu '%s'\n", arquivo);
+		fprintf(stderr, "Erro - ler_pgm: não abriu '%s'\n", arquivo);
 		return NULL; 
 	}
 
@@ -17,7 +17,7 @@ Imagem *ler_pgm(const char *arquivo)
 	char magic[4];
 	if (fscanf(f, "%3s", magic) != 1 || strcmp(magic, "P2") != 0) 
 	{
-		fprintf(stderr, "[ERRO] ler_pgm: arquivo não é P2 (era '%s')\n", magic);
+		fprintf(stderr, "Erro - ler_pgm: arquivo não é P2 (era '%s')\n", magic);
 		fclose(f);
 		return NULL; 
 	}
@@ -25,7 +25,7 @@ Imagem *ler_pgm(const char *arquivo)
 	// Consome restante da linha do magic
 	char buf[512];
 	if (fgets(buf, sizeof(buf), f) == NULL) {
-		fprintf(stderr, "[ERRO] ler_pgm: EOF apos magic\n");
+		fprintf(stderr, "Erro - ler_pgm: EOF apos magic\n");
 		fclose(f);
 		return NULL;
 	}
@@ -35,7 +35,7 @@ Imagem *ler_pgm(const char *arquivo)
 	while (1) {
 		if (fgets(buf, sizeof(buf), f) == NULL) 
 		{
-			fprintf(stderr, "[ERRO] ler_pgm: EOF ao ler dimensoes\n");
+			fprintf(stderr, "Erro - ler_pgm: EOF ao ler dimensoes\n");
 			fclose(f);
 			return NULL;
 		}
@@ -48,20 +48,20 @@ Imagem *ler_pgm(const char *arquivo)
 	int max_val = 0;
 	if (fscanf(f, "%d", &max_val) != 1) 
 	{
-		fprintf(stderr, "[ERRO] ler_pgm: falha ao ler max_val\n");
+		fprintf(stderr, "Erro - ler_pgm: falha ao ler max_val\n");
 		fclose(f);
 		return NULL;
 	}
 
 	// Aloca imagem
 	Imagem *img = (Imagem *)malloc(sizeof(Imagem)); 
-	if (!img) { fprintf(stderr, "[ERRO] ler_pgm: sem memoria\n"); fclose(f); return NULL; }
+	if (!img) { fprintf(stderr, "Erro - ler_pgm: sem memoria\n"); fclose(f); return NULL; }
 	img->largura = largura; 
 	img->altura = altura; 	
 
 	// Aloca TODOS os pixels num único bloco contíguo (1D)
 	img->pixels = (int *)malloc(altura * largura * sizeof(int)); 
-	if (!img->pixels) { free(img); fclose(f); fprintf(stderr, "[ERRO] ler_pgm: sem memoria\n"); return NULL; }
+	if (!img->pixels) { free(img); fclose(f); fprintf(stderr, "Erro - ler_pgm: sem memoria\n"); return NULL; }
 	
 	// Leitura dos dados mapeados linearmente
 	for (int y = 0; y < altura; y++) 
@@ -72,7 +72,7 @@ Imagem *ler_pgm(const char *arquivo)
 			if (fscanf(f, "%d", &img->pixels[y * largura + x]) != 1) 
 			{
 				free(img->pixels); free(img); fclose(f);
-				fprintf(stderr, "[ERRO] dados de pixel incompletos\n"); return NULL;
+				fprintf(stderr, "Erro: dados de pixel incompletos\n"); return NULL;
 			}
 		}
 	}
@@ -87,7 +87,7 @@ void salvar_pgm(const char *arquivo, const Imagem *img)
 	FILE *f = fopen(arquivo, "w");
 	if (!f) 
 	{
-		fprintf(stderr, "[ERRO] salvar_pgm: não abriu '%s'\n", arquivo);
+		fprintf(stderr, "Errro - salvar_pgm: não abriu '%s'\n", arquivo);
 		return;
 	}
 
